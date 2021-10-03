8396a244cce5dbf57cc9fae1f71b2cf8d834a26d
Beritens <34006542+Beritens@users.noreply.github.com>
2021-10-03 17:35:03 +0200

    prototyp
    it's spinning, whoop whoop!

---
+++ Assets/Player.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+    [SerializeField]
+    CharacterController controller;
+    Vector3 velocity;
+    [SerializeField]
+    float rotationSpeed;
+    [SerializeField]
+    float punchDist;
+    [SerializeField]
+    float punchRadius;
+    [SerializeField]
+    LayerMask spinningTopMask;
+    [SerializeField]
+    float PunchStrength;
+    [SerializeField]
+    float knockBack;
+
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Gravity();
+        LookToMouse();
+        if(Input.GetButtonDown("Fire1")){
+            Hit();
+        }
+        
+    }
+    public void Move(){
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+        
+
+        Vector3 move = new Vector3(x,0,z).normalized*Time.deltaTime*speed;
+        controller.Move(move);
+    }
+    void Gravity(){
+        velocity += Physics.gravity*Time.deltaTime;
+        controller.Move(velocity*Time.deltaTime);
+    }
+
+    void LookToMouse(){
+        Ray cameraRay =Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, new Vector3(0,1.5f,0)); // y value (1.5) eye level for better rotation, maybe change with ground floor later
+        float rayLength;
+
+        if(groundPlane.Raycast(cameraRay, out rayLength)){
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+            // lerp not good
+            // Quaternion rotation = Quaternion.LookRotation(new Vector3(pointToLook.x, transform.position.y, pointToLook.z)-transform.position);
+            // transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
+            Vector3 direction = new Vector3(pointToLook.x, transform.position.y, pointToLook.z) - transform.position;
+            Quaternion rotation1 = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation1, rotationSpeed*Time.deltaTime);
+        }
+    }
+    void Hit(){
+        Collider[] tops = Physics.OverlapSphere(transform.position+transform.forward*punchDist,punchRadius,spinningTopMask);
+        foreach (Collider top in tops)
+        {
+            SpinningTop st = top.GetComponent<SpinningTop>();
+            st.speedUp(PunchStrength);
+            st.Hit(transform.forward*knockBack);
+        }
+
+
+        // Debug.DrawRay(transform.position+transform.forward*punchDist,Vector3.forward *punchRadius,Color.green);
+        // Debug.DrawRay(transform.position+transform.forward*punchDist,-Vector3.forward *punchRadius,Color.green);
+        // Debug.DrawRay(transform.position+transform.forward*punchDist,Vector3.up *punchRadius,Color.green);
+        // Debug.DrawRay(transform.position+transform.forward*punchDist,-Vector3.up *punchRadius,Color.green);
+        // Debug.DrawRay(transform.position+transform.forward*punchDist,Vector3.right *punchRadius,Color.green);
+        // Debug.DrawRay(transform.position+transform.forward*punchDist,-Vector3.right *punchRadius,Color.green);
+    }
+}
---
+++ Assets/SpinningTop.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinningTop : MonoBehaviour
+{
+    [SerializeField]
+    float maxRpm = 1000f;
+    float rpm = 1000;
+    [SerializeField]
+    Transform outerSpin;
+    [SerializeField]
+    Transform childTransform;
+    [SerializeField]
+    Transform pivot;
+    [SerializeField]
+    float slowDownRate;
+    Rigidbody rb;
+
+/// <summary>
+/// Start is called on the frame when a script is enabled just before
+/// any of the Update methods is called the first time.
+/// </summary>
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        SlowDown();
+        Tilt();
+        childTransform.Rotate(Vector3.up * rpm * Time.deltaTime,Space.Self);
+        outerSpin.Rotate(Vector3.up * rpm * Time.deltaTime*0.5f,Space.Self);
+
+    }
+    public void speedUp(float force){
+        rpm= Mathf.Min(maxRpm,force+rpm);
+    }
+    void Tilt(){
+        pivot.localRotation = Quaternion.Euler(((1-(rpm/maxRpm))*45),0,0);
+    }
+    public void Hit(Vector3 force){
+        rb.AddForce(force,ForceMode.Impulse);
+    }
+    void SlowDown(){
+        rpm = Mathf.Max(rpm-slowDownRate * Time.deltaTime,0);
+        //rpm *= Mathf.Pow(slowDownRate,Time.deltaTime);
+    }
+}
