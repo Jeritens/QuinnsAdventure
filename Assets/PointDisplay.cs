9ff6c037d70017baf411bd8181ba8b6afa4f2df5
Jeritens <20913496+Jeritens@users.noreply.github.com>
2021-10-04 23:01:05 +0200

    cash life

--- Assets/PointDisplay.cs
+++ Assets/PointDisplay.cs
@@ -7,12 +7,14 @@ public class PointDisplay : MonoBehaviour
 {
     [SerializeField]
     TextMeshProUGUI display;
+    [SerializeField]
+    TextMeshProUGUI lifes;
 
 
     // Update is called once per frame
     void Update()
     {
-        display.text = GameManager.instance.points.ToString("0.0");
-
+        display.text = GameManager.instance.points.ToString("0");
+        lifes.text = "x" + GameManager.instance.extraLifes.ToString("0");
     }
 }
